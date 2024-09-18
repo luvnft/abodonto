518c6e00a68656c9fbd162a2263c67ef3caaf00d
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-09-18 23:00:41 +0200

    hero grid resize, responsive

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,17 +7,9 @@ export default function Hero({ hero }) {
   return (
     <section className="w-full pt-12 md:pt-24 lg:pt-32 bg-gray-900 text-white">
       <div className="container mx-auto px-4 md:px-6">
-        <div className="grid gap-6 lg:grid-cols-[1fr_600px] lg:gap-0 xl:grid-cols-[1fr_800px]">
-          <Image
-            src={'/ander.png'}
-            alt="Anderson Betioli"
-            height={600}
-            width={600}
-            className="mx-auto sm:w-full lg:order-last object-cover object-center"
-            priority={true}
-          />
-          <div className="flex flex-col justify-center space-y-4">
-            <div className="space-y-2">
+        <div className="pt-12 md:pt-0 grid gap-6 md:grid-cols-[1fr_auto] md:gap-12 lg:grid-cols-[1fr_400px] lg:gap-0 xl:grid-cols-[1fr_auto]">
+          <div className="flex flex-col justify-center space-y-4 items-center md:items-start md:pb-12 lg:pb-0">
+            <div className="space-y-2 text-center md:text-left">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
                 {hero.titulo}
               </h1>
@@ -39,6 +31,14 @@ export default function Hero({ hero }) {
               )}
             </div>
           </div>
+          <Image
+            src={'/ander.png'}
+            alt="Anderson Betioli"
+            height="800"
+            width="800"
+            className="mx-auto w-2/3 md:w-auto object-cover object-center"
+            priority={true}
+          />
         </div>
       </div>
     </section>
