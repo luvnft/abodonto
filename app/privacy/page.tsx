e85ae6c1378c14d370bc01a802b52818a27a7a41
Alexandre Betioli <apbetioli@gmail.com>
2024-09-19 09:33:18 +0200

    Add styles to privacy and terms page

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,8 +1,8 @@
 export default function Page() {
-    return (
-        <div className="prose">
-            <h1>Privacy Policy</h1>
-            <p>TODO</p>
-        </div>
-    )
+  return (
+    <div className="w-full pt-12 md:pt-24 lg:pt-32 prose">
+      <h1>Privacy Policy</h1>
+      <p>TODO</p>
+    </div>
+  );
 }
