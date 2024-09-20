c9ca5fcbe76b05ec5f13efb667ca4ab949f4098f
Alexandre Betioli <apbetioli@gmail.com>
2024-09-20 17:45:29 +0200

    Terms and Privacy pages

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,8 +1,15 @@
-export default function Page() {
+import client from '@/lib/graphQLClient';
+import textPageQuery from '../../queries/textPage.graphql';
+
+export default async function Page() {
+  const { data } = await client.query({
+    query: textPageQuery,
+    variables: { slug: 'politica-de-privacidade' },
+  });
+
   return (
-    <div className="w-full pt-12 md:pt-24 lg:pt-32 prose">
-      <h1>Privacy Policy</h1>
-      <p>TODO</p>
+    <div className="w-full m-auto pt-12 md:pt-24 lg:pt-32 prose">
+      <div dangerouslySetInnerHTML={{ __html: data.textPage?.text.html }} />
     </div>
   );
 }
--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,8 +1,15 @@
-export default function Page() {
+import client from '@/lib/graphQLClient';
+import textPageQuery from '../../queries/textPage.graphql';
+
+export default async function Page() {
+  const { data } = await client.query({
+    query: textPageQuery,
+    variables: { slug: 'termos-de-uso' },
+  });
+
   return (
-    <div className="w-full pt-12 md:pt-24 lg:pt-32 prose">
-      <h1>Terms Of Service</h1>
-      <p>TODO</p>
+    <div className="w-full mx-auto pt-12 md:pt-24 lg:pt-32 prose">
+      <div dangerouslySetInnerHTML={{ __html: data.textPage?.text.html }} />
     </div>
   );
 }
