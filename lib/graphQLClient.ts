d38b64d79c9850ee95f62f598cc378ab282a58be
Alexandre Betioli <apbetioli@gmail.com>
2024-09-21 11:21:28 +0200

    Remove config

--- lib/graphQLClient.ts
+++ lib/graphQLClient.ts
@@ -4,12 +4,6 @@ const client = new ApolloClient({
   ssrMode: typeof window === 'undefined',
   uri: process.env.GRAPHQL_CONTENT_ENDPOINT,
   cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: {
-      /* As we are only doing ssg lets not worry about caching now */
-      fetchPolicy: 'no-cache',
-    },
-  },
 });
 
 export default client;
