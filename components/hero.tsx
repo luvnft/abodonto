121e4b8d0152f69204117830b100598e814b5511
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-09-21 13:53:18 +0200

    refactor gradient, removed video brightness decrease

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,13 +6,13 @@ export default function HeroComponent({ hero }: { hero: Hero }) {
   return (
     <section className="w-full h-screen flex items-end justify-center text-white">
       <video
-        className="h-full w-full object-cover brightness-75"
+        className="h-full w-full object-cover"
         src="/hero.mp4"
         autoPlay
         loop
         muted
       ></video>
-      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent via-transparent to-black/95"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black via-gray-50/5"></div>
       <div className="absolute flex flex-col justify-center space-y-4 items-center text-center pb-12 xl:pb-32">
         <div className="space-y-2 text-center">
           <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
