8b2201faef4b4d0a4df7fc4674f253180978ec6c
Alexandre Betioli <apbetioli@gmail.com>
2024-09-23 12:34:37 +0200

    Fix codegen build

--- codegen.ts
+++ codegen.ts
@@ -7,11 +7,7 @@ const config: CodegenConfig = {
   generates: {
     'gql/': {
       preset: 'client',
-      plugins: [
-        'typescript',
-        'typescript-operations',
-        'typescript-react-apollo',
-      ],
+      plugins: [],
     },
   },
 };
