077f2310fad5661464f374393bb621121cd446a5
Alexandre Betioli <apbetioli@gmail.com>
2024-09-27 15:15:33 +0200

    Add wekhook for ISG revalidation

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,35 @@
+import { verifyWebhookSignature } from '@hygraph/utils';
+import { revalidatePath } from 'next/cache';
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.text();
+
+    const secret = process.env.HYGRAPH_WEBHOOK_SECRET;
+
+    if (secret) {
+      const signature = request.headers.get('gcms-signature') || '';
+
+      const isValid = verifyWebhookSignature({ body, signature, secret });
+
+      if (!isValid) {
+        console.log('[Next.js] Invalid signature.');
+        return new Response('Invalid signature.', {
+          status: 400,
+        });
+      }
+    }
+
+    console.log('[Next.js] Revalidating /');
+    revalidatePath('/');
+  } catch (error) {
+    const message = error instanceof Error ? error.message : String(error);
+    return new Response(`Webhook error: ${message}`, {
+      status: 400,
+    });
+  }
+
+  return new Response('Success!', {
+    status: 200,
+  });
+}
